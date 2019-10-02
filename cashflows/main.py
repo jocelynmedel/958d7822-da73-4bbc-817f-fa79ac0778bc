1fd191cf1aac0b185f8be38371e3082a1f1b9717
jocelynmedel <jocelynmedel@hotmail.com>
2019-10-01 22:35:19 -0500

    Exam JMR

--- cashflows/main.py
+++ cashflows/main.py
@@ -10,11 +10,21 @@ class Main(object):
         investment_project = InvestmentProject.from_csv(filepath=filepath, hurdle_rate=hurdle_rate)
         description = investment_project.describe()
         print(json.dumps(description, indent=4))
+        print('First, we have to understand this rates, the hurdle rate is the minimum rate that the someone expects to earn when investing')
+        print('and the IRR, is the interest rate at which the net present value of all cash flows is zero. ')
+        print('So, when the internal-rate of return is greater of equal than the hurdle rate, the investment is likely to be profit-making  ')
+        print('')
+        print('The net present value cant be negative, since is a sum of positive values,which are positive because the cashflow is a positive number divided by another positive number elevated to a positive time periods')
+        print('Investments must have a positive net present value, so is worth more today than the costs')
 
     @staticmethod
     def plot_investment(filepath, save="", show=False):
-        # TODO: 
-        raise NotImplementedError
+        print('JMR')
+        investment = InvestmentProject.from_csv(filepath=filepath)
+        fig = investment.plot(show=show)
+        if save:
+            fig.savefig("pic.png")
+        return
 
 if __name__ == "__main__":
     fire.Fire(Main)
--- cashflows/util.py
+++ cashflows/util.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-class Flow(object):
+class Cashflow(object):
 #Clase para Cashflow
 
     def __init__(self, **kwargs):
@@ -15,6 +16,7 @@ class Flow(object):
 
 class InvestmentProject (object):
 #Clase para investment project
+    RISK_FREE_RATE = 0.08
 
     def __init__(self, cashflows, hurdle_rate=RISK_FREE_RATE):
         cashflows_positions = {str(flow.t): flow for flow in cashflows}
@@ -39,8 +41,22 @@ class InvestmentProject (object):
         :param show: boolean that represents whether to run `plt.show()` or not.
         :return: matplotlib figure object.
         """
-        # TODO: implement plot method
-        raise NotImplementedError
+
+        amount_l = []
+        t_l = []
+        for obj in self.cashflows:
+            amount_l.append(obj.amount)
+            t_l.append(obj.t)
+        fig = plt.figure(1)
+        plt.bar(np.arange(len(amount_l)), amount_l)
+        plt.xticks(np.arange(len(t_l)), t_l)
+        plt.title ('CashFlows Bar Chart')
+        plt.xlabel ('t')
+        plt.ylabel('Amount')
+        if show:
+            plt.show()
+        return fig
+
 
 
     def net_present_value(self, interest_rate=None):
@@ -49,8 +65,12 @@ class InvestmentProject (object):
         :param interest_rate: represents the discount rate.
         :return: a number (currency) representing the net-present value.
         """
-        # TODO: implement net_present_value method
-        raise NotImplementedError
+        if interest_rate is None:
+            interest_rate = self.hurdle_rate
+        npv = 0
+        for i in self.cashflows:
+            npv += i.present_value(interest_rate=interest_rate)
+        return npv
 
     def equivalent_annuity(self, interest_rate=None):
         """ Equivalent Annuity
@@ -58,5 +78,17 @@ class InvestmentProject (object):
         :param interest_rate: represents the interest-rate used with the annuity calculations.
         :return: a number (currency) representing the equivalent annuity.
         """
-        # TODO: implement equivalent_annuity methdo
-        raise NotImplementedError
+        if interest_rate is None:
+            interest_rate = self.hurdle_rate
+        a = (self.net_present_value(interest_rate)*interest_rate)
+        b = 1- (1+interest_rate)**(-self.cashflow_max_position)
+        c = a/b
+        return c
+
+    def describe(self):
+        return {
+            "irr": self.internal_return_rate,
+            "hurdle-rate": self.hurdle_rate,
+            "net-present-value": self.net_present_value(interest_rate=None),
+            "equivalent-annuity": self.equivalent_annuity(interest_rate=None)
+        }
